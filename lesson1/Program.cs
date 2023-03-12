68880a4c8ad00bb527782af12f0652e700d46dd7
joennell <irinkinnik@gmail.com>
2023-03-12 23:55:24 +0700

    homework for lesson 1

---
+++ lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lesson1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var name = Console.ReadLine();
+        SayHello(name);
+    }
+
+    public static void SayHello (string name)
+    {
+        Console.WriteLine(name + ", Hello!");
+    }
+}
